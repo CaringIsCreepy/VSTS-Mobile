ce9bda1038db1b2e859d6de0b901d4d32eed43c3
Erik Elkins <erik.elkins@outlook.com>
2016-05-04 22:05:05 -0500

    Consolidating build outputs

--- gulpfile.js
+++ gulpfile.js
@@ -7,5 +7,5 @@ gulp.task('default', function () {
     var tsResult = tsProject.src()
         .pipe(ts(tsProject));
 
-    return tsResult.js.pipe(gulp.dest('release'));
+    return tsResult.js.pipe(gulp.dest('public/build'));
 });
