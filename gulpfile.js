2116984f43a31013b4561dc66a897f7ef2add0f4
Erik Elkins <erik.elkins@outlook.com>
2016-08-02 20:04:40 -0500

    Prod config changes

--- gulpfile.js
+++ gulpfile.js
@@ -2,23 +2,24 @@ var gulp = require('gulp');
 var ts = require('gulp-typescript');
 var merge = require('merge2'); 
 var sass = require('gulp-sass');
+var zip = require("gulp-zip");
 
 var tsProject = ts.createProject('tsconfig.json');
 
-gulp.task('dev', function () {        
-
-    gulp.src('./public/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./public/css'));
-        
-    gulp.src('./public/@angular2-material/button/*.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./public/css'));
-    
+gulp.task('dev', function () {
     var tsResult = tsProject.src()
         .pipe(ts(tsProject));
 
     return merge([
         tsResult.js.pipe(gulp.dest('./public'))
 	]);
+});
+
+gulp.task('prod', function () {
+    var tsResult = tsProject.src(['**/*.ts', '!**/node_modules/**'])
+                            .pipe(ts(tsProject));
+
+    return tsResult.js.pipe(gulp.dest('./public'))
+                      .pipe(zip("deploy.zip"))
+                      .pipe(gulp.dest("./"));
 });
