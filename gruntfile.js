f63ec51319e5996b5741128f01de8d5e72b33b20
Erik Elkins <erik.elkins@outlook.com>
2016-08-08 11:26:03 -0500

    Adding settings transformation

--- gruntfile.js
+++ gruntfile.js
@@ -6,12 +6,16 @@ module.exports = function (grunt) {
         ts: {
             default: {
                 src: ['**/*.ts', "!node_modules/**", "!public/node_modules/**"],
-                outdir: 'dist',
                 options: {
                     keepDirectoryHierarchy: true,
-                    module: 'amd',
+                    module: 'system',
+                    moduleResolution: "node",
                     target: 'es5',
-                    inlineSourceMap: true
+                    inlineSourceMap: true,
+                    sourceMap: true,
+                    emitDecoratorMetadata: true,
+                    experimentalDecorators: true,
+                    noImplicitAny: false
                 }
             }
         }
@@ -22,6 +26,21 @@ module.exports = function (grunt) {
 
     grunt.registerTask('prod', 'Log some stuff.', function () {
         grunt.task.run('ts');
+
+        var settings = grunt.file.read('public/app/core/settings.js');
+
+        settings = settings.replace("https://app.vssps.visualstudio.com/oauth2/authorize" +
+            "?client_id=45F0722C-BC39-447E-95C1-D3FE9B131C08" +
+            "&response_type=Assertion&state=test" +
+            "&scope=vso.code%20vso.build_execute%20vso.packaging_write%20vso.project_manage%20vso.release_execute%20vso.test%20vso.work_write" +
+            "&redirect_uri=http://127.0.0.1:80/", "https://app.vssps.visualstudio.com/oauth2/authorize" +
+            "?client_id=DE4A0837-6672-45EC-B9AF-66618A74BE74" +
+            "&response_type=Assertion&state=test" +
+            "&scope=vso.code%20vso.build_execute%20vso.packaging_write%20vso.project_manage%20vso.release_execute%20vso.test%20vso.work_write" +
+            "&redirect_uri=http://vsts-mobile.azurewebsites.net/");
+
+        grunt.file.write('public/app/core/settings.js', settings)
+
         grunt.task.run('zip');
     });
 };
