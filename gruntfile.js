cf0f6cd4a128ea3351e6f007bf2510246334dbfe
Erik Elkins <erik.elkins@outlook.com>
2016-09-03 15:36:29 -0500

    Fixing transform issues in gulp file

--- gruntfile.js
+++ gruntfile.js
@@ -42,17 +42,9 @@ module.exports = function (grunt) {
     grunt.registerTask('transformAzure', '', function () {
         var settings = grunt.file.read('./public/app/core/settings.js');
 
-        settings = settings.replace("https://app.vssps.visualstudio.com/oauth2/authorize" +
-            "?client_id=45F0722C-BC39-447E-95C1-D3FE9B131C08" +
-            "&response_type=Assertion&state=test" +
-            "&scope=vso.code%20vso.build_execute%20vso.packaging_write%20vso.project_manage%20vso.release_execute%20vso.test%20vso.work_write" +
-            "&redirect_uri=http://127.0.0.1:80/", "https://app.vssps.visualstudio.com/oauth2/authorize" +
-            "?client_id=DE4A0837-6672-45EC-B9AF-66618A74BE74" +
-            "&response_type=Assertion&state=test" +
-            "&scope=vso.code%20vso.build_execute%20vso.packaging_write%20vso.project_manage%20vso.release_execute%20vso.test%20vso.work_write" +
-            "&redirect_uri=http://vsts-mobile.azurewebsites.net/");
+        settings = settings.replace("client_id=45F0722C-BC39-447E-95C1-D3FE9B131C08", "client_id=DE4A0837-6672-45EC-B9AF-66618A74BE74");
 
-        settings = settings.replace("\"http://127.0.0.1/", "\"http://vsts-mobile.azurewebsites.net");
+        settings = settings.replace("http://127.0.0.1/", "http://vsts-mobile.azurewebsites.net");
 
         grunt.file.write('public/app/core/settings.js', settings)
     });
