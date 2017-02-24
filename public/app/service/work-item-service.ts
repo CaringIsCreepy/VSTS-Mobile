a0690d02af4520469a8272d618e6cfd83479f8cd
Erik Elkins <erik.elkins@outlook.com>
2017-02-23 21:49:34 -0600

    Adding iteration query

--- public/app/service/work-item-service.ts
+++ public/app/service/work-item-service.ts
@@ -44,6 +44,39 @@ export class WorkItemService {
         return subject.asObservable();
     }
 
+    getListFromWIQL(wiql: string): Observable<WorkItemList> {
+        var subject = new Subject<WorkItemList>();
+        var returnValue = new WorkItemList();
+        var server = this.window.localStorage.getItem('server');
+        let teamProject = this.window.localStorage.getItem('project_name');
+        let queryUrl = server + '/DefaultCollection/' + teamProject + '/_apis/wit/wiql?api-version=1.0';
+
+        this.oAuthHttp.post(queryUrl, {query: wiql}).subscribe(res => {
+            var result = res.json();
+            var idList = new Array<number>();
+
+            result.workItems.forEach(workItemJson => {
+                idList.push(workItemJson.id);
+            });
+
+            let workItemUrl = server + '/DefaultCollection/_apis/wit/WorkItems?ids=' + idList.join() + '&fields=System.Id,System.WorkItemType,System.Title,System.AssignedTo,System.State&api-version=1.0';
+
+            this.oAuthHttp.get(workItemUrl).subscribe(res => {
+                var result = res.json();
+
+                result.value.forEach(workItemJson => {
+                    var workItem = new WorkItem();
+                    workItem.populate(workItemJson);
+                    returnValue.push(workItem);
+                });
+
+                subject.next(returnValue);
+            });
+        });
+
+        return subject.asObservable();
+    }
+
     getItem(id: number): Observable<WorkItem> {
         let subject = new Subject<WorkItem>();
         let server = this.window.localStorage.getItem('server');
--- public/app/work_item_module/iteration-view.component.ts
+++ public/app/work_item_module/iteration-view.component.ts
@@ -1,20 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { IterationService } from '../service/iteration-service';
+import { WorkItemService } from '../service/work-item-service';
 import { Iteration } from '../business_object/iteration';
+import { WorkItemList } from '../business_object/work-item-list';
 import { Settings } from '../core/settings';
 import { OAuthHttp } from '../core/oauth-http';
 
 @Component({
     selector: "iterationView",
     templateUrl: "/app/work_item_module/iteration-view.html",
-    providers: [IterationService, OAuthHttp, Settings, { provide: Window, useValue: window }]
+    providers: [IterationService, WorkItemService, OAuthHttp, Settings, { provide: Window, useValue: window }]
 })
 export class IterationView implements OnInit {
     iteration: Iteration = new Iteration();
+    workItemList: WorkItemList;
 
     constructor(private route: ActivatedRoute,
-                private iterationService: IterationService) {
+                private iterationService: IterationService,
+                private workItemService: WorkItemService) {
 
     }
 
@@ -23,6 +27,12 @@ export class IterationView implements OnInit {
 
         this.iterationService.getById(id).subscribe(iteration => {
             this.iteration = iteration;
+
+            let wiql = `Select [State], [Title] From WorkItems Where [System.IterationPath] = '${this.iteration.path}'`;
+
+            this.workItemService.getListFromWIQL(wiql).subscribe(workItemList => {
+                this.workItemList = workItemList;
+            });
         });
     }
 }
