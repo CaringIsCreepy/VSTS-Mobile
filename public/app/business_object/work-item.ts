eada07edd69af0cefd924acfe883f9c888e0fb8c
Erik Elkins <erik.elkins@outlook.com>
2017-02-26 14:42:12 -0600

    More style changes

--- public/app/business_object/work-item.ts
+++ public/app/business_object/work-item.ts
@@ -3,6 +3,10 @@ export class WorkItem {
     rev: number;
     title: string;
     state: string;
+    type: string;
+    changedBy: string;
+    changedDate: Date;
+    assignedTo: string;
 
     populate(workItemJson: any) {
         this.id = workItemJson.id;
@@ -14,6 +18,10 @@ export class WorkItem {
     private populateFields(fieldsJson: any) {
         this.title = this.getField<string>(fieldsJson, 'System.Title');
         this.state = this.getField<string>(fieldsJson, 'System.State');
+        this.type = this.getField<string>(fieldsJson, 'System.WorkItemType');
+        this.changedBy = this.getField<string>(fieldsJson, 'System.ChangedBy');
+        this.changedDate = new Date(this.getField<string>(fieldsJson, 'System.ChangedDate'));
+        this.assignedTo = this.getField<string>(fieldsJson, 'System.AssignedTo');
     }
 
     private getField<T>(fieldsJson, field) : T {
